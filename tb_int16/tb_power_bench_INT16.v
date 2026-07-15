80c69da970e4fc4bbbbc8f2c46f69e0925d6410a
kingwangzzangyejun <skyaiden116@gmail.com>
2026-07-15 11:12:37 +0900

    Create tb_power_bench_INT16.v

---
+++ tb_int16/tb_power_bench_INT16.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+
+module tb_power_bench;
+    reg        CLK100MHZ = 1'b0;
+    reg        rst_btn   = 1'b1;
+    wire [7:0] led;
+    
+    always #5 CLK100MHZ = ~CLK100MHZ;
+    
+    power_bench_wrapper dut (
+        .CLK100MHZ (CLK100MHZ),
+        .rst_btn   (rst_btn),
+        .led       (led)
+    );
+    
+    initial begin
+        rst_btn = 1'b1;
+        repeat (10) @(posedge CLK100MHZ);
+        rst_btn = 1'b0;
+    end
+endmodule
+```
