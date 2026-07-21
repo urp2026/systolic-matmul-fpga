8fede72bef70d52f18de0dfeee76676079950caf
wogus825 <lthsso258@gmail.com>
2026-07-21 21:48:02 +0900

    Update tb_power_bench_INT16.v

--- tb_int16/tb_power_bench_INT16.v
+++ tb_int16/tb_power_bench_INT16.v
@@ -1,22 +1,37 @@
 `timescale 1ns/1ps
 
 module tb_power_bench;
-    reg        CLK100MHZ = 1'b0;
-    reg        rst_btn   = 1'b1;
+
+    reg CLK100MHZ = 1'b0;
+    reg rst_btn   = 1'b1;
+
     wire [7:0] led;
-    
+
     always #5 CLK100MHZ = ~CLK100MHZ;
-    
+
     power_bench_wrapper dut (
         .CLK100MHZ (CLK100MHZ),
         .rst_btn   (rst_btn),
         .led       (led)
     );
-    
+
     initial begin
+        $dumpfile("power_bench_int16.vcd");
+        $dumpvars(0, tb_power_bench);
+
         rst_btn = 1'b1;
-        repeat (10) @(posedge CLK100MHZ);
+
+        repeat (10)
+            @(posedge CLK100MHZ);
+
         rst_btn = 1'b0;
+
+        repeat (10000)
+            @(posedge CLK100MHZ);
+
+        $display("INT16 power bench simulation finished");
+
+        $finish;
     end
+
 endmodule
-```
